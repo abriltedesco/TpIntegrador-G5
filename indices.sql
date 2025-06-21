f70fb3c5b03f41f75976b30aa2d28d7c6de92b60
Abril Tedesco <167026793+abriltedesco@users.noreply.github.com>
2025-06-21 00:13:58 -0300

    Update indices.sql

--- indices.sql
+++ indices.sql
@@ -1,14 +1,15 @@
-CREATE INDEX index_producto_nombre ON producto(nombre);
+/*1*/ CREATE INDEX index_producto_nombre ON producto(nombre);
 
-ALTER TABLE usuario ADD UNIQUE INDEX index_emails_usuarios (email);
+/*2*/ ALTER TABLE usuario ADD UNIQUE INDEX index_emails_usuarios (email);
 CREATE UNIQUE INDEX index_emails_usuarios ON usuario(email);
 
+-- agregando mails
 ALTER TABLE usuario ADD COLUMN email varchar(100);
 UPDATE usuario SET email = 'juan.perez@gmail.com' WHERE idUsuario = 1;
-
+-- probando
 INSERT INTO usuario (idUsuario, username, nivel, reputacion, email)
 VALUES (41, 'nuevo.usuario', 'Normal', 100, 'juan.perez@gmail.com');
 
 
-CREATE INDEX index_publicacion_estado ON publicacion(idEstado);
+/*3*/  CREATE INDEX index_publicacion_estado ON publicacion(idEstado);
 SELECT * FROM publicacion WHERE idEstado IN (1, 3, 11, 12);
