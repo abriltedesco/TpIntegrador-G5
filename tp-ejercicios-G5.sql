f851ae2eb4a2367079bc95a93de63ac8bd9757d5
Abril Tedesco <167026793+abriltedesco@users.noreply.github.com>
2025-06-21 00:01:55 -0300

    Update tp-ejercicios-G5.sql

--- tp-ejercicios-G5.sql
+++ tp-ejercicios-G5.sql
@@ -170,6 +170,71 @@ end //
 delimiter ;
 
 #                                           ------------- STORED FUNCTIONS -------------
+/* 1) Crear la función comprarProducto que debe recibir un usuario comprador, una publicación, un medio de pago y un tipo de envío. Tiene que verificar que la publicación
+este activa, si no lo esta devuelve un mensaje ‘La publicación no está activa’. Si está activa verifica que se trate de una compra y no de una subasta. Si es una subasta
+devuelve el mensaje ‘Es una subasta’ y sino hace las modificaciones e inserts correspondientes */
+delimiter //
+create function comprarProducto(idUsuarioComprador int, id_Publicacion int, idMetodoPago int, idMetodoEnvio int) returns text deterministic
+begin
+	declare mensaje text default "-";
+    declare actividad boolean default 0;
+    declare esVentaDirecta boolean default 0;
+    declare esSubasta int;
+    declare idVenta int;
+    
+    select subastaId INTO esSubasta from publicacion where idPublicacion = id_Publicacion;
+    
+    IF esSubasta IS NULL THEN 
+		set esVentaDirecta = 1;
+	END IF;
+    
+    select estaActiva INTO actividad from publicacion
+    where idPublicacion = id_Publicacion;
+    
+	if actividad = 1 AND esVentaDirecta then
+		UPDATE publicacion set estaActiva = 0
+		WHERE publicacion.idPublicacion = id_Publicacion;
+		UPDATE publicacion set estado = 11
+		WHERE publicacion.idPublicacion = idPublicacion;
+        set idVenta = crearIdVenta();
+        insert into ventaDirecta value(idVenta, idMetodoPago, idMetodoEnvio);
+		INSERT INTO compra VALUE (idPublicacion, idVenta, idUsuarioComprador, null, null);
+		set mensaje = "Se ha actualizado el estado de la publicación";
+    else if actividad = 1 and esVentaDirecta = 0 then
+		set mensaje = "La publicación es una subasta ";
+	else if actividad = 0 then
+		set mensaje = "no esta activa.";
+	/*else 
+		set mensaje = "gdggf";*/
+    end if;
+    end if;
+    end if;
+    return mensaje;
+end//
+delimiter ;
+-- select comprarProducto(1, 5, 1, 1);
+
+
+/* 8. Crear la función responderPregunta que debe verificar que el id_vendedor recibido sea el id_vendedor asociado a la publicación sobre la cual se quiere responder, si es así se
+agrega la respuesta a la pregunta y devuelve el mensaje correspondiente */
+
+delimiter // 
+create function responderPregunta (idVendedor int, id_publicacion int, respuestaVendedor text) returns text  deterministic
+begin 
+	declare vendedor int;
+	declare mensaje text;
+	SELECT idUsuarioV into vendedor FROM publicacion WHERE idPublicacion = id_publicacion;
+	IF vendedor = idVendedor THEN 
+		UPDATE comentario SET respuesta = respuestaVendedor WHERE idPublicacion = id_publicacion;
+        set mensaje = "se ha agregado la respuesta exitosamente ";
+	ELSE 
+		set mensaje = "no es posible agregar la respuesta";
+	END IF;
+    return mensaje;
+end //
+delimiter ;
+-- SELECT responderPregunta(16, 13, "Es de acero inoxidable");
+
 
 #                                                 ------------- VISTAS ------------- 
 /* 1) Crear una vista que muestre todas las preguntas de publicaciones activas que aún no
@@ -190,8 +255,7 @@ where week(fechaPublicacion) = week(current_date())
 group by 1,2 
 order by cantidad desc
 limit 10;
-
-SELECT * FROM topCategoriasEstaSemana;
+-- SELECT * FROM topCategoriasEstaSemana;
 
 /* 3) Crear una vista que muestre las publicaciones que se encuentran en tendencia el día de
 hoy. Estas serán las que tengan mayor cantidad de preguntas */
@@ -204,8 +268,8 @@ group by 1
 order by cantidad desc
 limit 10;
 
-UPDATE publicacion SET fechaPublicacion = current_date() WHERE idPublicacion = 1;
-SELECT * FROM publicacionesTendenciaHoy;
+-- UPDATE publicacion SET fechaPublicacion = current_date() WHERE idPublicacion = 1;
+-- SELECT * FROM publicacionesTendenciaHoy;
 
 /* 4) Crear una vista que muestre el nombre del vendedor con mayor reputación por
 categoría. Se debe mostrar nombre del vendedor y nombre de la categoría */
